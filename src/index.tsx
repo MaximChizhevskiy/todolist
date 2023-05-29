894c0ff774661b590080f1d4a22cd1f831d081fd
Maxim Chizhevskiy <max15.91@mail.ru>
2023-05-29 16:45:25 +0600

    add_HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -9,11 +9,11 @@ import { Switch } from "@mui/material"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 )
 
 // If you want to start measuring performance in your app, pass a function
